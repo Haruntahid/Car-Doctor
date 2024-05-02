408f3db7b7770b2c2bad89370c7ad99db3b8ec69
haruntahid <tahidtaha997@gmail.com>
2024-05-03 00:28:30 +0600

    added footer

--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Layout() {
   return (
-    <div className="container mx-auto">
-      <Header />
-      <Outlet />
-    </div>
+    <>
+      <div className="container mx-auto">
+        <Header />
+        <Outlet />
+      </div>
+      <Footer />
+    </>
   );
 }
 
