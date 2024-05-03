178b175cdf82865d5d9c32c352710bfdab2079f5
haruntahid <tahidtaha997@gmail.com>
2024-05-03 15:37:06 +0600

    login page added

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -3,7 +3,7 @@ import { FaGoogle } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-function SignUp() {
+function Login() {
   return (
     <>
       <div className="flex items-center">
@@ -11,19 +11,10 @@ function SignUp() {
           <img src={img} alt="" />
         </div>
         {/* from */}
-        <div className="w-1/2">
+        <div className="w-1/2 ">
           <div className="w-full max-w-lg p-16 space-y-3 rounded-xl bg-white text-[#444] border-2 border-[#D0D0D0]">
-            <h1 className="text-4xl font-bold text-center">Sign Up</h1>
-            <form noValidate="" action="" className="space-y-6">
-              <div className="space-y-2 text-sm mt-10">
-                <label className="block text-[#444]  font-semibold">Name</label>
-                <input
-                  type="text"
-                  name="name"
-                  placeholder="Your name"
-                  className="w-full px-4 py-3 rounded-md border border-[#E8E8E8]  text-[#444] "
-                />
-              </div>
+            <h1 className="text-4xl font-bold text-center">Login</h1>
+            <form className="space-y-6">
               <div className="space-y-1 text-sm">
                 <label className="block text-[#444]  font-semibold">
                   Email
@@ -47,12 +38,12 @@ function SignUp() {
                 />
               </div>
               <button className="block w-full p-3 text-center rounded-sm text-white bg-[#FF3811]">
-                Sign Up
+                Sign In
               </button>
             </form>
             <div className="flex items-center pt-4 space-x-1">
               <div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
-              <p className="px-3 text-sm text-gray-400">Or Sign Up with</p>
+              <p className="px-3 text-sm text-gray-400">Or Sign In with</p>
               <div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
             </div>
             <div className="flex justify-center space-x-4">
@@ -71,8 +62,8 @@ function SignUp() {
             </div>
             <p className="text-[18px] text-center sm:px-6 text-[#737373]">
               Don&apos;t have an account?
-              <Link to={"/login"} className="underline text-[#FF3811]">
-                Sign up
+              <Link to={"/signup"} className="underline text-[#FF3811]">
+                Sign In
               </Link>
             </p>
           </div>
@@ -82,4 +73,4 @@ function SignUp() {
   );
 }
 
-export default SignUp;
+export default Login;
--- client/src/Router/Router.jsx
+++ client/src/Router/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../Pages/Home";
 import SignUp from "../Pages/SignUp";
+import Login from "../Pages/Login";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
       {
         path: "/signup",
         element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
       },
     ],
   },
